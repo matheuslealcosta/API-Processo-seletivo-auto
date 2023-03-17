cfec5900d7f9f984a05a30bbd62dc44f8b1a7b28
MatGod135 <matheuslealcosta@icloud.com>
2023-03-17 11:21:20 -0300

    implementa sistema com googleapi passando dados do forms para sheets

--- src/controllers/InscricaoController.ts
+++ src/controllers/InscricaoController.ts
@@ -1,16 +1,52 @@
 import { PrismaClient } from '@prisma/client';
 import { Request, Response } from 'express';
-import { google } from 'googleapis';
+import {google} from 'googleapis';
+
 
 const prisma = new PrismaClient();
-const SPREADSHEET_ID = 'SPREADSHEET_ID'; // Substitua pelo ID da sua planilha
-const RANGE = 'Sheet1'; // Substitua pela range que deseja inserir os dados
-const CREDENTIALS = require('./credentials.json');
 
 class InscricaoController {
-  static store = async (req: Request, res: Response) => {
-  
+  static getAuthSheets = async (req: Request, res: Response) => {
     
+    const auth = new google.auth.GoogleAuth({
+      keyFile: 'credentials.json',
+      scopes:"https://www.googleapis.com/auth/spreadsheets"
+    });
+
+    const client = await auth.getClient();
+
+    const googleSheets = google.sheets({
+      version: "v4",
+      auth: client
+    });
+
+    const spreadsheetId = "1iHKoZaOXPGgLmT7tOg9Ob2FOm3oKfe-jWH7f_o4fnyg"
+
+    return {
+      auth,
+      client,
+      googleSheets,
+      spreadsheetId,
+    };
+    
+  };
+
+  static store = async (req: Request, res: Response) => {
+    const { googleSheets, auth, spreadsheetId } = await InscricaoController.getAuthSheets(req, res);
+
+    const {nome, email, curso, telefone } = req.body;
+
+    const row = await googleSheets.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range: "Página1",
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: [[nome, email, curso, telefone]]
+      },
+    });
+  
+    res.send(row.data);
   };
 }
 
